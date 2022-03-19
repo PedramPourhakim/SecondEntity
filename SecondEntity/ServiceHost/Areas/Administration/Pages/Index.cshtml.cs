1dcaf4cf118476d545df6077276a105805c715bc
PedramPourhakim <shemuel1226@gmail.com>
2022-03-19 15:45:03 +0330

    Admin Panel Razor Page Successfully Run

--- SecondEntity/ServiceHost/Areas/Administration/Pages/Index.cshtml.cs
+++ SecondEntity/ServiceHost/Areas/Administration/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ServiceHost.Areas.Administration
+namespace ServiceHost.Areas.Administration.Pages
 {
     public class IndexModel : PageModel
     {
