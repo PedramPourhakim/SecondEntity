650f0e78c2c555748b3b5096acf13581288a9c50
PedramPourhakim <shemuel1226@gmail.com>
2022-03-19 11:29:58 +0330

    Shop Management Configure Services For Interfaces & Their Implementations & Connection Strings Added

---
+++ SecondEntity/ShopManagement.Configuration/ShopManagementBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application;
+using ShopManagement.Application.Contracts.ProductCategoryContracts;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EfCore;
+using ShopManagement.Infrastructure.EfCore.Repository;
+using System;
+
+namespace ShopManagement.Configuration
+{
+    public class ShopManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection Services,string connectionstring)
+        {
+            Services.AddScoped<IProductCategoryApplication,ProductCategoryApplication>();
+            Services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+            Services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+            Services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+            Services.AddDbContext<ShopContext>(x=>x.UseSqlServer(connectionstring));
+        }
+    }
+}
