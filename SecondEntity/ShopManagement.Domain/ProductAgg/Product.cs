dd36696d6bb0cefe16a3189d6b018372c567adc0
PedramPourhakim <shemuel1226@gmail.com>
2022-03-23 14:49:13 +0430

    Product Model Added To Domain

---
+++ SecondEntity/ShopManagement.Domain/ProductAgg/Product.cs
@@ -0,0 +1,77 @@
+﻿using _0_Framework.Domain;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Domain.ProductAgg
+{
+    public class Product:EntityBase
+    {
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+        public double UnitPrice { get; private set; }
+        public bool IsInStock { get; private set; }
+        public string ShortDescription { get; private set; }
+        public string Description { get; private set; }
+        public string Picture { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+        public long CategoryId { get; private set; }
+        public string Slug { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string Keywords { get; private set; }
+        public ProductCategory Category { get; private set; }
+
+        public Product(string Name, string Code, 
+            double UnitPrice, string ShortDescription, 
+            string Description, string Picture,
+            string PictureAlt, string PictureTitle, 
+            long CategoryId, string Slug, 
+            string MetaDescription, string Keywords)
+        {
+            this.Name = Name;
+            this.Code = Code;
+            this.UnitPrice = UnitPrice;
+            this.ShortDescription = ShortDescription;
+            this.Description = Description;
+            this.Picture = Picture;
+            this.PictureAlt = PictureAlt;
+            this.PictureTitle = PictureTitle;
+            this.CategoryId = CategoryId;
+            this.Slug = Slug;
+            this.MetaDescription = MetaDescription;
+            this.Keywords = Keywords;
+        }
+        public void Edit(string Name, string Code,
+          double UnitPrice, string ShortDescription,
+          string Description, string Picture,
+          string PictureAlt, string PictureTitle,
+          long CategoryId, string Slug,
+          string MetaDescription, string Keywords)
+        {
+            this.Name = Name;
+            this.Code = Code;
+            this.UnitPrice = UnitPrice;
+            this.ShortDescription = ShortDescription;
+            this.Description = Description;
+            this.Picture = Picture;
+            this.PictureAlt = PictureAlt;
+            this.PictureTitle = PictureTitle;
+            this.CategoryId = CategoryId;
+            this.Slug = Slug;
+            this.MetaDescription = MetaDescription;
+            this.Keywords = Keywords;
+        }
+        public void InStock()
+        {
+            IsInStock = true;
+        }
+        public void NotInStock()
+        {
+            IsInStock = false;
+        }
+    }
+}
