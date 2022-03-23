e7aaf05b0671b4884a05b9a434a4ee0a49ffb6b6
PedramPourhakim <shemuel1226@gmail.com>
2022-03-23 15:08:35 +0430

    Private Sets Removed From Create Product Model Because They Are Unnecessary

--- SecondEntity/ShopManagement.Application.Contracts/ProductContracts/CreateProduct.cs
+++ SecondEntity/ShopManagement.Application.Contracts/ProductContracts/CreateProduct.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopManagement.Application.Contracts.ProductContracts
+﻿namespace ShopManagement.Application.Contracts.ProductContracts
 {
     public class CreateProduct
     {
-        public string Name { get; private set; }
-        public string Code { get; private set; }
-        public double UnitPrice { get; private set; }
-        public string ShortDescription { get; private set; }
-        public string Description { get; private set; }
-        public string Picture { get; private set; }
-        public string PictureAlt { get; private set; }
-        public string PictureTitle { get; private set; }
-        public long CategoryId { get; private set; }
-        public string Slug { get; private set; }
-        public string MetaDescription { get; private set; }
-        public string Keywords { get; private set; }
+        public string Name { get;  set; }
+        public string Code { get;  set; }
+        public double UnitPrice { get;  set; }
+        public string ShortDescription { get;  set; }
+        public string Description { get;  set; }
+        public string Picture { get;  set; }
+        public string PictureAlt { get;  set; }
+        public string PictureTitle { get;  set; }
+        public long CategoryId { get;  set; }
+        public string Slug { get;  set; }
+        public string MetaDescription { get;  set; }
+        public string Keywords { get;  set; }
     }
 }
