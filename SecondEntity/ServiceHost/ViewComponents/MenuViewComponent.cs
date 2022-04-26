54f62b9a5a56c1b581910c80d3bcaf2d688f33ba
PedramPourhakim <shemuel1226@gmail.com>
2022-04-26 14:39:54 +0430

    Footer & Menu Move To Separate View Component

---
+++ SecondEntity/ServiceHost/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using _01_Query.Contract.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHost.ViewComponents
+{
+    public class MenuViewComponent :ViewComponent
+    {
+        private readonly IProductCategoryQuery productCategoryQuery;
+
+        public MenuViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            this.productCategoryQuery = productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
