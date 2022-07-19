519b8faff8aa406519e0f0206af4fb7c672e985b
Pedram <Shemuel1226@gmail.com>
2022-07-19 15:04:20 +0430

    Account Context Project Modified To Compatible Version

--- SecondEntity/ShopManagement.Infrastructure.EfCore/Repository/OrderRepository.cs
+++ SecondEntity/ShopManagement.Infrastructure.EfCore/Repository/OrderRepository.cs
@@ -1,6 +1,6 @@
-﻿using AccountManagement.Infrastructure.EfCore;
-using _0_Framework.Application;
+﻿using _0_Framework.Application;
 using _0_Framework.Infrastructure;
+using AccountManagement.Infrastructure.EfCore;
 using ShopManagement.Application.Contracts.Order;
 using ShopManagement.Domain.OrderAgg;
 using System.Collections.Generic;
@@ -8,12 +8,13 @@ using System.Linq;
 
 namespace ShopManagement.Infrastructure.EfCore.Repository
 {
-    public class OrderRepository:RepositoryBase<long,Order>,IOrderRepository
+    public class OrderRepository : RepositoryBase<long, Order>, IOrderRepository
     {
         private readonly AccountContext accountContext;
         private readonly ShopContext shopContext;
         public OrderRepository(ShopContext shopContext
-            ):base(shopContext)
+            ,AccountContext accountContext) 
+            : base(shopContext)
         {
             this.shopContext = shopContext;
             this.accountContext = accountContext;
@@ -23,7 +24,7 @@ namespace ShopManagement.Infrastructure.EfCore.Repository
         {
             var order = shopContext.Orders.Select(x =>
               new { x.PayAmount, x.Id }).FirstOrDefault(
-                x=>x.Id==id);
+                x => x.Id == id);
             if (order != null)
                 return order.PayAmount;
             return 0;
@@ -33,31 +34,31 @@ namespace ShopManagement.Infrastructure.EfCore.Repository
         {
 
             var accounts = accountContext.Accounts.
-                Select(x => new {x.Id,x.FullName }).ToList();
+                Select(x => new { x.Id, x.FullName }).ToList();
             var query = shopContext.Orders.Select(x =>
-              new OrderViewModel 
-              { 
-                  Id=x.Id,
-                  AccountId=x.AccountId,
-                  DiscountAmount=x.DiscountAmount,
-                  IsCanceled=x.IsCanceled,
-                  IsPaid=x.IsPaid,
-                  IssueTrackingNo=x.IssueTrackingNo,
-                  PayAmount=x.PayAmount,
-                  PaymentMethodId=x.PaymentMethod,
-                  RefId=x.RefId,
-                  TotalAmount=x.TotalAmount,
-                  CreationDate=x.CreationDate.ToFarsi(),
+              new OrderViewModel
+              {
+                  Id = x.Id,
+                  AccountId = x.AccountId,
+                  DiscountAmount = x.DiscountAmount,
+                  IsCanceled = x.IsCanceled,
+                  IsPaid = x.IsPaid,
+                  IssueTrackingNo = x.IssueTrackingNo,
+                  PayAmount = x.PayAmount,
+                  PaymentMethodId = x.PaymentMethod,
+                  RefId = x.RefId,
+                  TotalAmount = x.TotalAmount,
+                  CreationDate = x.CreationDate.ToFarsi(),
               });
             query = query.Where(x => x.IsCanceled ==
               searchModel.IsCanceled);
-            if (searchModel.AccountID >0)
+            if (searchModel.AccountID > 0)
             {
-                query = query.Where(x => x.AccountId 
+                query = query.Where(x => x.AccountId
                 == searchModel.AccountID);
             }
 
-            var orders= query.OrderByDescending(x=>x.Id)
+            var orders = query.OrderByDescending(x => x.Id)
                 .ToList();
         }
     }
